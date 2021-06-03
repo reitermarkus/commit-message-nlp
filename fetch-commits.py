9ee919093ee951cfc42e8f4ae340008233cbd78c
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2021-06-03 02:25:01 +0200

    Query repositories with "PyGithub".

--- fetch-commits.py
+++ fetch-commits.py
@@ -1,50 +1,17 @@
 #!/usr/bin/env python3
 
-import bq_helper
+import os
+from github import Github
 
-# https://www.kaggle.com/poonaml/analyzing-3-million-github-repos-using-bigquery
-github_repos = bq_helper.BigQueryHelper(
-  active_project= "bigquery-public-data",
-  dataset_name = "github_repos",
-)
+token = os.environ['GITHUB_TOKEN']
 
-# print(github_repos.list_tables())
-#
-# print(github_repos.head('languages'))
-#
-# print(github_repos)
+g = Github(token)
 
-from google.cloud import bigquery
-import pandas as pd
+languages = ['rust', 'python', 'ruby', 'c', 'haskell', 'c#', 'javascript']
 
-client = bigquery.Client()
+repository_register = {}
 
-QUERY = """
-SELECT message
-FROM `bigquery-public-data.github_repos.commits`
-WHERE LENGTH(message) > 10 AND LENGTH(message) <= 80
-LIMIT 10000
-"""
-
-query_size = github_repos.estimate_query_size(QUERY)
-
-print(f'Query size: {query_size} GB')
-
-exit()
-
-query_job = client.query(QUERY)
-
-iterator = query_job.result(timeout=30)
-rows = list(iterator)
-
-for v in rows:
-  commit_message = list(v.values())[0]
-
-  if commit_message.startswith('chore'):
-    print(commit_message)
-
-# commit_messages = pd.DataFrame(data=[list(x.values()) for x in rows], columns=list(rows[0].keys()))
-#
-# # Look at the first 10 headlines
-# print(commit_messages.head(1000))
+for language in languages:
+  repositories = g.search_repositories(query='stars:>10000 language:' + language).get_page(0)
+  repository_register[language] = repositories[:5]
 
