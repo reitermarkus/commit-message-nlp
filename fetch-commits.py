82db42c98c569441253ab3a19d6f47cab5d325f0
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-06-03 20:11:49 +0200

    Query some commits and save to .csv

--- fetch-commits.py
+++ fetch-commits.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import csv
 from github import Github
 
 token = os.environ['GITHUB_TOKEN']
@@ -15,3 +16,15 @@ for language in languages:
   repositories = g.search_repositories(query='stars:>10000 language:' + language).get_page(0)
   repository_register[language] = repositories[:5]
 
+for l in languages:
+  all_commits = []
+
+  for repository in repository_register[l]:
+    commits = [ (repository.name, repr(c.commit.message)) for c in repository.get_commits().get_page(0)[:20] ]
+    all_commits.extend(commits)
+
+  with open(f'results/{l}.csv', 'w+', encoding='utf-8') as out:
+    csv_out = csv.writer(out, lineterminator='\n')
+    csv_out.writerow(['repository','message'])
+    for row in all_commits:
+      csv_out.writerow(row)
