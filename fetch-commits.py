f43e026c7fd32c618252dfb3beb37c4c2df4dd6f
Markus Reiter <me@reitermark.us>
2021-06-07 01:18:39 +0200

    Sleep until rate limit is reset.

--- fetch-commits.py
+++ fetch-commits.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import os
+from datetime import datetime
 from pathlib import Path
+from time import sleep
 import csv
 from github import Github
 
@@ -22,54 +24,72 @@ languages = [
   'swift',
 ]
 
-repository_register = {}
+def check_rate_limit(resource, show=False):
+  rate_limit = g.get_rate_limit()
+
+  if resource == 'core':
+    rate_limit = rate_limit.core
+
+  if resource == 'search':
+    rate_limit = rate_limit.search
+
+  remaining = rate_limit.remaining
+  limit = rate_limit.limit
+
+  if show:
+    print(f'Rate limit ({resource}): {remaining}/{limit}')
+
+  if remaining / limit >= 0.1:
+    return
+
+  current_time = datetime.utcnow()
+  reset_time = rate_limit.reset
+
+  reset_seconds = (reset_time - current_time).total_seconds()
+
+  if reset_seconds < 0:
+    return
+
+  print(f'Rate limit resets in {reset_seconds} seconds, sleeping until then.')
+  sleep(reset_seconds)
 
 for language in languages:
   commit_count = 0
-  repo_page = 0
+  check_rate_limit(resource='search', show=True)
+  for repo in g.search_repositories(query=f'language:{language}', sort='stars', order='desc'):
+    print(f'Fetching repo {repo.full_name}')
+    path = Path('results')/'repo'/f'{repo.full_name}.csv'
 
-  while commit_count < 100000:
-    repositories = g.search_repositories(query=f'language:{language}', sort='stars', order='desc').get_page(repo_page)
+    if path.exists():
+      print(f'Already contained in cache.')
+      continue
 
-    if not repositories:
-      break
+    all_commits = []
 
-    for repo in repositories:
-      print(f'Fetching repo {repo.full_name}')
-      path = Path('results')/'repo'/f'{repo.full_name}.csv'
+    check_rate_limit(resource='core', show=True)
+    for c in repo.get_commits():
+      commit = c.commit
 
-      if path.exists():
-        print(f'Already contained in cache.')
+      merge_commit = len(commit.parents) > 1
+      if merge_commit:
         continue
 
-      commit_page = 0
-      all_commits = []
+      all_commits.append((repo.full_name, repr(c.commit.message)))
+      if len(all_commits) >= 10000:
+        break
 
-      while len(all_commits) < 10000:
-        commits = repo.get_commits().get_page(commit_page)
+      check_rate_limit(resource='core')
 
-        if not commits:
-          break
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, 'w+', encoding='utf-8') as out:
+      csv_out = csv.writer(out, lineterminator='\n')
+      csv_out.writerow(['repository', 'message'])
+      for row in all_commits:
+        csv_out.writerow(row)
 
-        for c in commits:
-          commit = c.commit
+    print(f'Fetched {len(all_commits)} commits.')
+    commit_count += len(all_commits)
+    if commit_count >= 100000:
+      break
 
-          merge_commit = len(commit.parents) > 1
-          if merge_commit:
-            continue
-
-          all_commits.append((repo.full_name, repr(c.commit.message)))
-
-        commit_page += 1
-
-      path.parent.mkdir(parents=True, exist_ok=True)
-      with open(path, 'w+', encoding='utf-8') as out:
-        csv_out = csv.writer(out, lineterminator='\n')
-        csv_out.writerow(['repository', 'message'])
-        for row in all_commits:
-          csv_out.writerow(row)
-
-      print(f'Fetched {len(all_commits)} commits.')
-      commit_count += len(all_commits)
-
-    repo_page += 1
+    check_rate_limit(resource='search', show=True)
