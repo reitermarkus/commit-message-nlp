0677e8b05b73a692e8ab89f8e81019de5519fdf9
Markus Reiter <me@reitermark.us>
2021-06-14 00:52:40 +0200

    Add logistic regression.

--- helpers.py
+++ helpers.py
@@ -1,7 +1,10 @@
 import re
+from time import sleep, perf_counter
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score, accuracy_score
 
 stopwords = stopwords.words('english')
 
@@ -28,3 +31,28 @@ def tokenize(commit_message):
   words = [word for word in words if not word in stopwords]
 
   return words
+
+def test(item):
+  i, ((training_index, test_index), source_data, target_data) = item
+
+  start = perf_counter()
+
+  source_training_data, source_test_data = source_data[training_index], source_data[test_index]
+  target_training_data, target_test_data = target_data[training_index], target_data[test_index]
+
+  log_reg = LogisticRegression(solver='newton-cg')
+  log_reg.fit(source_training_data, target_training_data)
+
+  target_prediction_data = [round(value) for value in log_reg.predict(source_test_data)]
+
+  accuracy = accuracy_score(target_test_data, target_prediction_data)
+  f1_micro = f1_score(target_test_data, target_prediction_data, average='micro')
+  f1_macro = f1_score(target_test_data, target_prediction_data, average='macro')
+
+  stop = perf_counter()
+  print(f'Fold {i} took {stop - start:0.3f} seconds.')
+  print("  Accuracy:", accuracy)
+  print("  F1 micro:", f1_micro)
+  print("  F1 macro:", f1_macro)
+
+  return [accuracy, f1_micro, f1_macro]
